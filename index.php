c3bd3cfd46fdf771c3db467849df192d04925785
Matthew Cherry <matiwisnia16@gmail.com>
2024-05-18 16:45:51 +0200

    Add config file, cleanup

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 
+use Ahc\Cli\Application;
+use weather\WeatherCommand;
+
 require_once('./vendor/autoload.php');
+require_once('./config.php');
+
+$app = new Application('WeatherApp', 'v0.0.1');
+
+$app->add(new WeatherCommand);
+
+$app->handle($_SERVER['argv']);
