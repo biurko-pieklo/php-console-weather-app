6be2521dfed14907b4c2112ebb52d6525c267dfa
Matthew Cherry <matiwisnia16@gmail.com>
2024-05-29 20:37:23 +0200

    initial command structure

--- src/WeatherCommand.php
+++ src/WeatherCommand.php
@@ -2,83 +2,66 @@
 
 namespace weather;
 
-class WeatherCommand extends Ahc\Cli\Input\Command
+use Ahc\Cli\Input\Command;
+use Ahc\Cli\IO\Interactor;
+
+class WeatherCommand extends Command
 {
     public function __construct()
     {
-        parent::__construct('init', 'Init something');
-
-        $this
-            ->argument('<arrg>', 'The Arrg')
-            ->argument('[arg2]', 'The Arg2')
-            ->option('-a --apple', 'The Apple')
-            ->option('-b --ball', 'The ball')
-            // Usage examples:
-            ->usage(
-                // append details or explanation of given example with ` ## ` so they will be uniformly aligned when shown
-                '<bold>  init</end> <comment>--apple applet --ball ballon <arggg></end> ## details 1<eol/>' .
-                // $0 will be interpolated to actual command name
-                '<bold>  $0</end> <comment>-a applet -b ballon <arggg> [arg2]</end> ## details 2<eol/>'
-            );
+        parent::__construct('weather', 'Get the weather');
     }
 
     // This method is auto called before `self::execute()` and receives `Interactor $io` instance
-    public function interact(Ahc\Cli\IO\Interactor $io) : void
+    public function interact(Interactor $io) : void
     {
-        // Collect missing opts/args
-        if (!$this->apple) {
-            $this->set('apple', $io->prompt('Enter apple'));
+        $io->write('Dostępne miasta:', true);
+
+        $json = json_decode(file_get_contents(__DIR__.'/../res/cities.json'), true);
+
+        foreach ($json as $name => $coords) {
+            $io->write($name, true);
         }
 
-        if (!$this->ball) {
-            $this->set('ball', $io->prompt('Enter ball'));
+        $io->write('wpisz "exit", aby skończyć', true);
+
+        $selected = [];
+
+        while (true) {
+            $city = $io->prompt('Wybierz miasto');
+
+            if ($city == 'exit') {
+                break;
+            } else if (array_key_exists($city, $json)) {
+                $api = new OpenWeatherAPI($json[$city]['lat'], $json[$city]['lng']);
+                $data = $api->handleIO($io);
+                $selected[$city] = $data;
+            } else {
+                $io->write('Nie możesz wybrać takiego miasta.', true);
+            }
         }
 
-        // ...
+        $this->set('cities', $selected);
+
+        while (true) {
+            $format = $io->prompt('Do jakiego formatu zapisać dane? (PDF, JSON, XML)');
+
+            if (!in_array(strtoupper($format), AVAILABLE_FORMATS)) {
+                $io->write('Wybierz poprawny format.', true);
+            } else {
+                break;
+            }
+        }
+
+        $this->set('format', strtoupper($format));
     }
 
-    // When app->handle() locates `init` command it automatically calls `execute()`
-    // with correct $ball and $apple values
-    public function execute($ball, $apple)
+    public function execute(array $cities, string $format)
     {
         $io = $this->app()->io();
 
-        $io->write('Apple ' . $apple, true);
-        $io->write('Ball ' . $ball, true);
-
-        // more codes ...
-
-        // If you return integer from here, that will be taken as exit error code
+        foreach ($cities as $city => $data) {
+            $io->write($city, true);
+        }
     }
 }
-
-class OtherCommand extends Ahc\Cli\Input\Command
-{
-    public function __construct()
-    {
-        parent::__construct('other', 'Other something');
-    }
-
-    public function execute()
-    {
-        $io = $this->app()->io();
-
-        $io->write('Other command');
-
-        // more codes ...
-
-        // If you return integer from here, that will be taken as exit error code
-    }
-}
-
-// Init App with name and version
-$app = new Ahc\Cli\Application('App', 'v0.0.1');
-
-// Add commands with optional aliases`
-$app->add(new InitCommand, 'i');
-$app->add(new OtherCommand, 'o');
-
-// Set logo
-$app->logo('Ascii art logo of your app');
-
-$app->handle($_SERVER['argv']); // if argv[1] is `i` or `init` it executes InitCommand
